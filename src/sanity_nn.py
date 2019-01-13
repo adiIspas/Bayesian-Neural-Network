a4be649a9f8df4fc9b39baeca47751cb6e741b78
Adrian Ispas <adrian.ispas@ab4.systems>
2019-01-13 17:17:45 +0200

    A stable version

--- src/sanity_nn.py
+++ src/sanity_nn.py
@@ -2,11 +2,18 @@ import numpy as np
 import pymc as pm
 from matplotlib import pyplot as plt
 
-### SANITY CHECK ###
-X = np.random.randn(100, 2)
-Y = np.tanh(X[:, 0] + X[:, 1])
-Y = 1. / (1. + np.exp(-(Y + Y)))
-Y = Y > 0.5
+# def generate_data(size):
+#     X = np.random.randn(size, 2)
+#     Y = np.tanh(X_train[:, 0] + X_train[:, 1])
+#     Y = 1. / (1. + np.exp(-(Y + Y)))
+#     Y = Y > 0.5
+
+# TRAIN #
+number_of_samples = 10000
+X_train = np.random.randn(100, 2)
+Y_train = np.tanh(X_train[:, 0] + X_train[:, 1])
+Y_train = 1. / (1. + np.exp(-(Y_train + Y_train)))
+Y_train = Y_train > 0.5
 
 w11 = pm.Normal('w11', mu=0., tau=1.)
 w12 = pm.Normal('w12', mu=0., tau=1.)
@@ -15,8 +22,8 @@ w22 = pm.Normal('w22', mu=0., tau=1.)
 w31 = pm.Normal('w31', mu=0., tau=1.)
 w32 = pm.Normal('w32', mu=0., tau=1.)
 
-x1 = X[:, 0]
-x2 = X[:, 1]
+x1 = X_train[:, 0]
+x2 = X_train[:, 1]
 
 x3 = pm.Lambda('x3', lambda w1=w11, w2=w12: np.tanh(w1 * x1 + w2 * x2))
 x4 = pm.Lambda('x4', lambda w1=w21, w2=w22: np.tanh(w1 * x1 + w2 * x2))
@@ -27,13 +34,12 @@ def sigmoid(x=w31 * x3 + w32 * x4):
     return 1. / (1. + np.exp(-x))
 
 
-# x5 = pm.Lambda('x5', lambda w1=w31, w2=w32: sigmoid)
-y = pm.Bernoulli('y', sigmoid, observed=True, value=Y)
-
+y = pm.Bernoulli('y', sigmoid, observed=True, value=Y_train)
 model = pm.Model([w11, w12, w21, w22, w31, w32, y])
 inference = pm.MCMC(model)
 
-inference.sample(10000)
+inference.sample(number_of_samples)
+y_pred_train = pm.Bernoulli('y_pred_train', sigmoid)
 
 traces = inference.trace("w11")[:]
 plt.hist(traces)
@@ -64,3 +70,18 @@ traces = inference.trace("w32")[:]
 plt.hist(traces)
 plt.title('Posterior of w32')
 plt.show()
+
+# TEST #
+X_test = np.random.randn(100, 2)
+Y_test = np.tanh(X_test[:, 0] + X_test[:, 1])
+Y_test = 1. / (1. + np.exp(-(Y_test + Y_test)))
+Y_test = Y_test > 0.5
+
+x1 = X_test[:, 0]
+x2 = X_test[:, 1]
+
+inference.sample(number_of_samples)
+y_pred_test = pm.Bernoulli('y_pred_test', sigmoid)
+
+print('Accuracy on train data = {}%'.format((y_pred_train.value == Y_train).mean() * 100))
+print('Accuracy on test data = {}%'.format((y_pred_test.value == Y_test).mean() * 100))
